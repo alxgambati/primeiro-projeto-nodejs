1e866144f8b6da0f1bcae3bb3ac7a686eaaef5e5
alxgambati <gambati@hotmail.com>
2020-05-05 18:45:22 -0300

    partial commit

--- src/database/migrations/1588706552890-CreateUsers.ts
+++ src/database/migrations/1588706552890-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1588706552890 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
