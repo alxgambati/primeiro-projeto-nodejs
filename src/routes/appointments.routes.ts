e5e4dc50b9b8bb37f48bed2d2df2c3a68f519978
alxgambati <gambati@hotmail.com>
2020-05-05 16:10:32 -0300

    N02-Primeiro-Projeto-NodeJS-com-appointments-services

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,33 +1,35 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
-import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
-const appointmentRepository = new AppointmentRepository();
-
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentRepository.all();
-
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentRepository.find();
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(appointmentRepository);
+    const createAppointment = new CreateAppointmentService();
 
-    const appointment = createAppointment.execute({ provider, date: parsedDate });
+    const appointment = await createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
 
     return response.json(appointment);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
-
 
 export default appointmentsRouter;
